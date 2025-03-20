08895d55bad5565bf934cb3452e593fe65b01e09
BogdanFAndrei <bogdan.fandrei@yahoo.com>
2025-03-20 22:47:17 +0000

    Format code

---
+++ todo/src/Signin/styles.js
@@ -0,0 +1,42 @@
+const SigninStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 40,
+    alignItems: "center",
+    },
+    input: {
+      borderWidth: 1,
+      borderColor: "black",
+      padding: 10,
+      margin: 10,
+      width: "100%",
+    },
+    button: {
+      backgroundColor: "blue",
+  
+      width: "100%",
+      borderRadius: 10,
+    },
+    text: {
+      margin: 5,
+      color: "black",
+      fontSize: 20,
+      alignSelf: "flex-start",
+    },
+    title: {
+      fontSize: 24,
+      fontWeight: "bold",
+      marginBottom: 20,
+      textDecorationLine: "underline",
+    },
+    button2: {
+      fontSize: 20,
+      textAlign: "center",
+      color: "blue",
+      padding: 10,
+      margin: 10,
+      width: "100%",
+    },
+  });
+
+export default SigninStyles;
