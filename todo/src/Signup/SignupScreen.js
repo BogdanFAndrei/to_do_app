12e2ed2199d77cda79147e677a9602805f6a84f0
BogdanFAndrei <bogdan.fandrei@yahoo.com>
2025-03-20 22:48:02 +0000

    Format code

--- todo/src/Signup/SignupScreen.js
+++ todo/src/Signup/SignupScreen.js
@@ -1,30 +1,46 @@
-import React from 'react'
-import { View, Text, TextInput, TouchableOpacity, Button } from 'react-native'
-import SignupStyles from './styles'
+import React from "react";
+import { View, Text, TextInput, TouchableOpacity, Button } from "react-native";
+import SignupStyles from "./styles";
 
 const SignupScreen = ({ navigation }) => {
-    return (<View style={SignupStyles.container}>
+  return (
+    <View style={SignupStyles.container}>
+      <Text style={SignupStyles.title}>Signup Screen</Text>
 
-            <Text style={SignupStyles.title}>Signup Screen</Text>
-            <Text style={SignupStyles.text}>Username</Text>  
-            <TextInput style={SignupStyles.input} placeholder="Please enter your username" />
-            <Text style={SignupStyles.text}>Email</Text>  
-            <TextInput style={SignupStyles.input} placeholder="Please enter your email" />
-            <Text style={SignupStyles.text}>Password</Text>
-            <TextInput style={SignupStyles.input} placeholder="Please enter your password" />
+      <Text style={SignupStyles.text}>Username</Text>
 
+      <TextInput
+        style={SignupStyles.input}
+        placeholder="Please enter your username"
+      />
 
+      <Text style={SignupStyles.text}>Email</Text>
 
-            <Button style= {SignupStyles.button} title="Signup" onPress={() => navigation.navigate('Signin')} />
-                <TouchableOpacity onPress={() => navigation.navigate('Signin')}><Text style={SignupStyles.button2}>Already have an account? {'\n'} Signin</Text></TouchableOpacity>
-        </View>
-    )
-}
+      <TextInput
+        style={SignupStyles.input}
+        placeholder="Please enter your email"
+      />
 
+      <Text style={SignupStyles.text}>Password</Text>
 
+      <TextInput
+        style={SignupStyles.input}
+        placeholder="Please enter your password"
+      />
 
+      <Button
+        style={SignupStyles.button}
+        title="Signup"
+        onPress={() => navigation.navigate("Signin")}
+      />
+      
+      <TouchableOpacity onPress={() => navigation.navigate("Signin")}>
+        <Text style={SignupStyles.button2}>
+          Already have an account? {"\n"} Signin
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
-
-
-
-export default SignupScreen
+export default SignupScreen;
