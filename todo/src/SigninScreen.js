98da7abe073e44fa43559af300ca15ee964eb3ce
BogdanFAndrei <bogdan.fandrei@yahoo.com>
2025-03-20 22:28:00 +0000

    modifying sign in screen

--- todo/src/SigninScreen.js
+++ todo/src/SigninScreen.js
@@ -1,10 +1,19 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, Button } from 'react-native'
+import { useState } from 'react'
 
-const SigninScreen = () => {
-    return (
-        <View>
-            <Text>Signin Screen</Text>
+const SigninScreen = ({ navigation }) => {
+    return (<View style={styles.container}>
+
+            <Text style={ styles.title}>Sign In Screen</Text>
+            <Text style={styles.text}>Username or Email</Text>  
+            <TextInput style={styles.input} placeholder="Please enter your username or email" />
+            <Text style={styles.text}>Password</Text>
+            <TextInput style={styles.input} placeholder="Please enter your password" />
+
+
+
+            <Button style= {styles.button} title="Signin" onPress={() => navigation.navigate('Signin')} />
+            <TouchableOpacity onPress={() => navigation.navigate('Signin')}><Text style={styles.button2}>Don't have an account? {'\n'} Go back to Signup</Text></TouchableOpacity>
         </View>
     )
 }
@@ -12,7 +21,47 @@ const SigninScreen = () => {
 
 
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 40,
+        alignItems: 'center',
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: 'black',
+        padding: 10,
+        margin: 10,
+        width: '100%',
+    },
+    button: {
+        backgroundColor: 'blue',
+     
+        width: '100%',
+        borderRadius: 10,
+    },
+    text: {
+       
+        margin: 5,
+        color: 'black',
+        fontSize: 20,
+        alignSelf: 'flex-start',
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 20,
+        textDecorationLine: 'underline',
+    },
+    button2: {
+        fontSize:20,
+        textAlign: 'center',
+        color: 'blue',
+        padding: 10,
+        margin: 10,
+        width: '100%',
+    }
+})
 
 
 export default SigninScreen
