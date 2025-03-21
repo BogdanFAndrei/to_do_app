99ae65223356335b86b40a40ba5885925982aa0e
BogdanFAndrei <bogdan.fandrei@yahoo.com>
2025-03-21 15:45:25 +0000

    Add AuthContext for authentication state management

--- todo/src/context/AuthContext.js
+++ todo/src/context/AuthContext.js
@@ -1,51 +1,62 @@
 import createDataContext from "./createDataContext";
 import trackerApi from "../api/tracker";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const authReducer = (state, action) => {
-    switch (action.type) {
-        case "signup":
-            return {...state, token: action.payload};
-        case "add_error":
-            return {...state, errorMessage: action.payload};
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case "signup":
+      return { errorMessage: "", token: action.payload };
+    case "add_error":
+      return { ...state, errorMessage: action.payload };
+    default:
+      return state;
+  }
+};
 
 const signup = (dispatch) => {
-    return async (username, email, password) => {
-        try {
-            const response = await trackerApi.post("/signup", {username, email, password});
-            console.log(response.data);
-            dispatch({type: "signup", payload: response.data});
-        } catch (error) {
-            dispatch({type: "add_error", payload: "Something went wrong with signup"});
-        }
+  return async ({username, email, password}, callback) => {
+    try {
+      const response = await trackerApi.post("/signup", {
+        username,
+        email,
+        password,
+      });
+      await AsyncStorage.setItem("token", response.data.token);
+      dispatch({ type: "signup", payload: response.data });
+      if (callback) {
+        callback();
+      }
+    } catch (error) {
+      dispatch({
+        type: "add_error",
+        payload: "Something went wrong with signup",
+      });
     }
-}
+  };
+};
 
 const signin = (dispatch) => {
-    return async (usernameOrEmail, password) => {
-        try {
-            const response = await trackerApi.post("/signin", {usernameOrEmail, password});
-            dispatch({type: "signin", payload: response.data});
-        } catch (error) {
-            console.log(error);
-        }
+  return async (usernameOrEmail, password) => {
+    try {
+      const response = await trackerApi.post("/signin", {
+        usernameOrEmail,
+        password,
+      });
+      dispatch({ type: "signin", payload: response.data });
+    } catch (error) {
+      console.log(error);
     }
-}
+  };
+};
 
 const signout = (dispatch) => {
-    return () => {
-        dispatch({type: "signout"});
-    }
-}
+  return () => {
+    dispatch({ type: "signout" });
+  };
+};
 
-export const {Provider, Context} = createDataContext(
-    authReducer,
-    {signup, signin, signout},
-    {isSigneIn: false, errorMessage: "" },
-
-
-    
-)
+export const { Provider, Context } = createDataContext(
+  authReducer,
+  { signup, signin, signout },
+  { token: null, errorMessage: "" }
+);
