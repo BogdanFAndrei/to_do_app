a0f3a1a8cdab4d61ba0c3d6d637cb3fef38ddeac
BogdanFAndrei <bogdan.fandrei@yahoo.com>
2025-03-21 15:06:44 +0000

    Add context providers for state management

---
+++ todo/src/context/AuthContext.js
@@ -0,0 +1,49 @@
+import createDataContext from "./createDataContext";
+import trackerApi from "../api/tracker";
+
+const authReducer = (state, action) => {
+    switch (action.type) {
+        case "signup":
+            return {...state, token: action.payload};
+        default:
+            return state;
+    }
+}
+
+const signup = (dispatch) => {
+    return async (username, email, password) => {
+        try {
+            const response = await trackerApi.post("/signup", {username, email, password});
+            console.log(response.data);
+            dispatch({type: "signup", payload: response.data});
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+const signin = (dispatch) => {
+    return async (usernameOrEmail, password) => {
+        try {
+            const response = await trackerApi.post("/signin", {usernameOrEmail, password});
+            dispatch({type: "signin", payload: response.data});
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+const signout = (dispatch) => {
+    return () => {
+        dispatch({type: "signout"});
+    }
+}
+
+export const {Provider, Context} = createDataContext(
+    authReducer,
+    {signup, signin, signout},
+    {isSigneIn: false },
+
+
+    
+)
