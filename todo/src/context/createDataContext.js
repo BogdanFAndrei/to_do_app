6590031a115c0ceeed45edf9b220aba2289352ab
BogdanFAndrei <bogdan.fandrei@yahoo.com>
2025-03-22 20:06:21 +0000

    docs: add JSDoc documentation to createDataContext module

--- todo/src/context/createDataContext.js
+++ todo/src/context/createDataContext.js
@@ -1,8 +1,26 @@
+/**
+ * @fileoverview Factory function for creating a React Context with reducer and actions
+ * @module context/createDataContext
+ */
+
 import React, {useReducer} from "react";
 
+/**
+ * Creates a React Context with a reducer and bound actions
+ * @param {Function} reducer - Reducer function for state management
+ * @param {Object} actions - Object containing action creator functions
+ * @param {Object} defaultValue - Initial state value
+ * @returns {Object} Object containing Context and Provider components
+ */
 export default (reducer, actions, defaultValue) => {
     const Context = React.createContext();
     
+    /**
+     * Provider component that wraps the context and provides state and actions
+     * @param {Object} props - Component props
+     * @param {React.ReactNode} props.children - Child components to be wrapped
+     * @returns {JSX.Element} Context Provider component
+     */
     const Provider = ({children}) => {
         const [state, dispatch] = useReducer(reducer, defaultValue);
 
