4e7740f0de7576b3dc528b8b5ce6b82b3c2876f3
BogdanFAndrei <bogdan.fandrei@yahoo.com>
2025-03-22 20:05:23 +0000

    docs: add JSDoc documentation to navigationRef module

--- todo/src/navigationRef.js
+++ todo/src/navigationRef.js
@@ -1,11 +1,29 @@
+/**
+ * @fileoverview Navigation reference module for handling navigation outside of React components
+ * @module navigationRef
+ */
+
 import { NavigationActions } from "react-navigation";
 
+/**
+ * Global navigator reference
+ * @type {Object}
+ */
 let navigator;
 
+/**
+ * Sets the global navigator reference
+ * @param {Object} nav - Navigation object from React Navigation
+ */
 export const setNavigator = (nav) => {
     navigator = nav;
 };
 
+/**
+ * Navigates to a specified route with optional parameters
+ * @param {string} routeName - Name of the route to navigate to
+ * @param {Object} [params] - Optional parameters to pass to the route
+ */
 export const navigate = (routeName, params) => {
     navigator.dispatch( 
         NavigationActions.navigate({
