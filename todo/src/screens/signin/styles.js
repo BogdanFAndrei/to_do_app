6b89e6f632c33bfaa25303e66e441ce08b915e9b
BogdanFAndrei <bogdan.fandrei@yahoo.com>
2025-03-24 15:49:01 +0000

    styles for sign in page

--- todo/src/screens/signin/styles.js
+++ todo/src/screens/signin/styles.js
@@ -3,42 +3,21 @@ import { StyleSheet } from "react-native";
 const SigninStyles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 40,
-    alignItems: "center",
-    },
-    input: {
-      borderWidth: 1,
-      borderColor: "black",
-      padding: 10,
-      margin: 10,
-      width: "100%",
-    },
-    button: {
-      backgroundColor: "blue",
-  
-      width: "100%",
-      borderRadius: 10,
-    },
-    text: {
-      margin: 5,
-      color: "black",
-      fontSize: 20,
-      alignSelf: "flex-start",
-    },
-    title: {
-      fontSize: 24,
-      fontWeight: "bold",
-      marginBottom: 20,
-      textDecorationLine: "underline",
-    },
-    button2: {
-      fontSize: 20,
-      textAlign: "center",
-      color: "blue",
-      padding: 10,
-      margin: 10,
-      width: "100%",
-    },
-  });
+    justifyContent: 'center',
+    marginBottom: 250,
+  },
+  input: {
+    backgroundColor: '#F8F8F8',
+    borderRadius: 8,
+    padding: 12,
+    fontSize: 16,
+    color: '#333',
+  },
+  link: {
+    color: 'blue',
+    textAlign: 'center',
+  },
+});
+
 
 export default SigninStyles;
