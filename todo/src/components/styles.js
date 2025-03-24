cf6c3fdd844d6379c21fd7283ca6707aba91ce6f
BogdanFAndrei <bogdan.fandrei@yahoo.com>
2025-03-24 15:50:09 +0000

    Added the styles to be used for loginflow

---
+++ todo/src/components/styles.js
@@ -0,0 +1,39 @@
+import { StyleSheet } from 'react-native';
+
+export const AuthFormStyles = StyleSheet.create({
+  input: {
+    margin: 10,
+  },
+  errorMessage: {
+    fontSize: 16,
+    color: 'red',
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+  button: {
+    margin: 10,
+  },    
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: '600',
+  },
+  container: {
+    margin: 15,
+    marginTop: 140,
+  },    
+  label: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 5,
+    textAlign: 'center',
+    marginTop: 10,
+  },
+
+});
