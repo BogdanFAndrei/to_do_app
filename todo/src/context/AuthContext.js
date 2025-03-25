204b37abd38dfe3cb31971b5d2059a7410b4587a
BogdanFAndrei <bogdan.fandrei@yahoo.com>
2025-03-25 14:32:35 +0000

    Modified context allowing user to only user username or email for signin

--- todo/src/context/AuthContext.js
+++ todo/src/context/AuthContext.js
@@ -24,6 +24,8 @@ const authReducer = (state, action) => {
       return { errorMessage: "", token: action.payload };
     case "clear_error_message":
       return { ...state, errorMessage: "" };
+    case "try_local_signin":
+      return { ...state, token: action.payload };
     default:
       return state;
   }
@@ -34,6 +36,17 @@ const authReducer = (state, action) => {
  * @param {Function} dispatch - Redux dispatch function
  * @returns {Function} Signup function that handles user registration
  */
+
+const tryLocalSignin = (dispatch) => async () => {
+  const token = await AsyncStorage.getItem('token');
+  if (token){
+    dispatch({type: 'signin', payload: token})
+    navigate('mainFlow')
+  }else{
+    navigate('Signup')
+  }
+}
+
 const clearErrorMessage = (dispatch) => {
   return () => {
     dispatch({ type: "clear_error_message" });
@@ -42,9 +55,16 @@ const clearErrorMessage = (dispatch) => {
 
 const signup =
   (dispatch) =>
-  async ({ usernameOrEmail, password }) => {
+  async ({ username, email, password, error }) => {
+    if (error) {
+      dispatch({
+        type: "add_error",
+        payload: error,
+      });
+      return;
+    }
     try {
-      const response = await trackerApi.post("/signup", { usernameOrEmail, password });
+      const response = await trackerApi.post("/signup", { username, email, password });
       await AsyncStorage.setItem("token", response.data.token);
       dispatch({ type: "signup", payload: response.data.token });
 
@@ -57,18 +77,29 @@ const signup =
     }
   };
 
- 
-  
-
 const signin = (dispatch) => {
-  return async ({ username, email, password }) => {
-    try{
-      const usernameOrEmail =  username || email;
-      const response = await trackerApi.post('/signin', {usernameOrEmail, password})
+  return async ({ username, email, password, error }) => {
+    if (error) {
+      dispatch({
+        type: "add_error",
+        payload: error,
+      });
+      return;
+    }
+    try {
+      const usernameOrEmail = username || email;
+      if (!usernameOrEmail || !password) {
+        dispatch({
+          type: "add_error",
+          payload: "Please provide either username or email and password",
+        });
+        return;
+      }
+      const response = await trackerApi.post('/signin', { usernameOrEmail, password });
       await AsyncStorage.setItem("token", response.data.token);
       dispatch({ type: "signin", payload: response.data.token });
       navigate("mainFlow");
-    }catch (err){
+    } catch (err) {
       console.log(err);
       dispatch({
         type: "add_error",
@@ -86,6 +117,6 @@ const signout = (dispatch) => {
 
 export const { Provider, Context } = createDataContext(
   authReducer,
-  { signin, signout, signup, clearErrorMessage },
+  { signin, signout, signup, clearErrorMessage, tryLocalSignin },
   { token: null, errorMessage: "" }
 );
