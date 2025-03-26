3ef5aa0af61131874cc0c31550920dbc7b6a52bf
BogdanFAndrei <bogdan.fandrei@yahoo.com>
2025-03-26 15:00:25 +0000

    docs: add documentation to babel configuration file

--- todo/babel.config.js
+++ todo/babel.config.js
@@ -1,3 +1,17 @@
+/**
+ * Babel Configuration File
+ * 
+ * This file configures Babel for the Expo React Native application.
+ * It sets up the necessary presets and plugins for transpiling modern JavaScript
+ * and React Native specific features.
+ * 
+ * Configuration includes:
+ * - babel-preset-expo: Preset for Expo React Native applications
+ * - react-native-reanimated/plugin: Plugin for React Native Reanimated animations
+ * 
+ * @module babel.config
+ */
+
 module.exports = function (api) {
   api.cache(true);
   return {
