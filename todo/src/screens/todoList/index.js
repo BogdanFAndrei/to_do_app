1933c5b7608f0470882e6e3c2a0127c8dcc6e721
BogdanFAndrei <bogdan.fandrei@yahoo.com>
2025-03-26 15:00:22 +0000

    docs: add documentation to todoList screen component

--- todo/src/screens/todoList/index.js
+++ todo/src/screens/todoList/index.js
@@ -1,3 +1,18 @@
+/**
+ * Todo List Screen Component
+ * 
+ * This screen displays the main list of todo items for the authenticated user.
+ * It serves as the primary interface for viewing and managing todo items.
+ * 
+ * Features:
+ * - Displays a list of all todo items
+ * - Allows navigation to todo item details
+ * - Provides options to mark items as complete/incomplete
+ * - Integrates with the todo management context
+ * 
+ * @component
+ */
+
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 
@@ -9,10 +24,6 @@ const ToDoListScreen = () => {
     )
 }
 
-
-
-
 const styles = StyleSheet.create({})
-
 
 export default ToDoListScreen
