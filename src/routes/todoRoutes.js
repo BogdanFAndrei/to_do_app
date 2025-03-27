ed450632b13c214dd53237f9933bd79e64307948
BogdanFAndrei <bogdan.fandrei@yahoo.com>
2025-03-27 09:34:19 +0000

    Update todo routes to use proper MongoDB schema fields

--- src/routes/todoRoutes.js
+++ src/routes/todoRoutes.js
@@ -1,34 +1,69 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const requireAuth = require('../middlewares/requireAuth');
-
-const ToDo = mongoose.model('ToDo');
+const ToDo = require('../models/ToDo');
 
 const router = express.Router();
 
 router.use(requireAuth);
 
-router.get('/todos', async (req, res) => {
-  const todos = await ToDo.find({ userId: req.user._id });
-
-  res.send(todos);
+// Get all todos for a user
+router.get('/', async (req, res) => {
+  try {
+    const todos = await ToDo.find({ userId: req.user._id }).sort({ createdAt: -1 });
+    res.json(todos);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
-router.post('/todos', async (req, res) => {
-  const { title, description } = req.body;
-
-  if (!title || !description) {
-    return res
-      .status(422)
-      .send({ error: 'You must provide a title and description' });
-  }
+// Create a new todo
+router.post('/', async (req, res) => {
+  const todo = new ToDo({
+    userId: req.user._id,
+    title: req.body.title,
+    content: req.body.content,
+    color: req.body.color || '#000000'
+  });
 
   try {
-    const todo = new ToDo({ title, description, userId: req.user._id });
-    await todo.save();
-    res.send(todo);
-  } catch (err) {
-    res.status(422).send({ error: err.message });
+    const newTodo = await todo.save();
+    res.status(201).json(newTodo);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// Update a todo
+router.patch('/:id', async (req, res) => {
+  try {
+    const todo = await ToDo.findOne({ _id: req.params.id, userId: req.user._id });
+    if (!todo) {
+      return res.status(404).json({ message: 'Todo not found' });
+    }
+
+    if (req.body.title) todo.title = req.body.title;
+    if (req.body.content) todo.content = req.body.content;
+    if (req.body.color) todo.color = req.body.color;
+    if (req.body.completed !== undefined) todo.completed = req.body.completed;
+
+    const updatedTodo = await todo.save();
+    res.json(updatedTodo);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// Delete a todo
+router.delete('/:id', async (req, res) => {
+  try {
+    const todo = await ToDo.findOneAndDelete({ _id: req.params.id, userId: req.user._id });
+    if (!todo) {
+      return res.status(404).json({ message: 'Todo not found' });
+    }
+    res.json({ message: 'Todo deleted' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 });
 
